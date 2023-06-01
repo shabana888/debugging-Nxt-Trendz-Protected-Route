3ea4452fe5e178fc16e1fdf57a0ae93fdbed333c
shabana888 <shaikshabana.btc@gmail.com>
2023-06-01 06:23:13 +0000

    debugging-protected-route

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,15 @@
+import {Route, Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const token = Cookies.get('jwt_token')
+
+  if (token === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  return <Route {...props} />
+  //   7 spread operator is not used
+}
+
+export default ProtectedRoute
